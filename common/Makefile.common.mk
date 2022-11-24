2fc271e8c3cadfa941285d06093678118bbfc4ce
Adam Shank <42580017+ashank07@users.noreply.github.com>
2022-11-23 21:18:05 -0500

    New prow cluster name

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -24,8 +24,8 @@ INSTALL_HOOKS:
 # config docker
 ############################################################
 PROJECT ?= oceanic-guard-191815
-ZONE    ?= us-west1-a
-CLUSTER ?= prow
+ZONE    ?= us-east5-c
+CLUSTER ?= bedrock-prow
 
 activate-serviceaccount:
 ifdef GOOGLE_APPLICATION_CREDENTIALS
